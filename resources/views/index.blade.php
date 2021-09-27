2dbeb19282b6dd5be9c1edc0b5ae3dbf1305a253
omartins-zs <gabrielmatheus209@gmail.com>
2021-09-27 17:39:23 -0300

    Ajustando View,Controllers,Routes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,79 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('app')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('titulo', 'Página Inicial')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
-    <title>E-Diaristas</title>
-</head>
-
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('diaristas.index') }}">E-Diaristas</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('diaristas.index') }}">Lista de diaristas</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <div class='container'>
-        <h1>Lista de Diaristas</h1>
-        <table class=" table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Telefone</th>
-                    <th scope="col">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($diaristas as $diarista)
-                <tr>
-                    <th scope="row">{{ $diarista->id }}</th>
-                    <td>{{ $diarista->nome_completo }}</td>
-                   <td>{{$diarista->telefone}}</td> 
-                    <td></td>
-                </tr>
-                @empty
-                <tr>
-                    <th></th>
-                    <td>Nenhum registro cadastrado</td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                @endforelse
-
-            </tbody>
-        </table>
-        <a  class="btn btn-success"href="{{ route('diaristas.create') }}">Nova Diaristas</a>
-
-    </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-    -->
-</body>
-
-</html>
+@section('conteudo')
+<h1>Lista de Diaristas</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($diaristas as $diarista)
+        <tr>
+            <th scope="row">{{ $diarista->id }}</th>
+            <td>{{ $diarista->nome_completo }}</td>
+            <td>{{$diarista->telefone}}</td>
+            <td> <a href="{{ route('diaristas.edit', $diarista) }}" class="btn btn-primary">Atualizar</a>
+                <a href="{{ route('diaristas.destroy', $diarista) }}" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja apagar?')">Apagar</a>
+            </td>
+        </tr>
+        @empty
+        <tr>
+            <th></th>
+            <td>Nenhum registro cadastrado</td>
+            <td></td>
+            <td></td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+<a href="{{ route('diaristas.create') }}" class="btn btn-success">Nova Diarista</a>
+@endsection
